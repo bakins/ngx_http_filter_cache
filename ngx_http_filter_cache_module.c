bf1556fa8f329ec39ec862e41311830a3d8611bc
Brian Akins <brian@akins.org>
2012-03-22 19:00:01 -0400

    actually declare conf this time

--- ngx_http_filter_cache_module.c
+++ ngx_http_filter_cache_module.c
@@ -722,7 +722,7 @@ filter_cache_send(ngx_http_request_t *r)
 static ngx_int_t
 ngx_http_filter_cache_rewrite_handler(ngx_http_request_t *r)
 {
-    conf = ngx_http_get_module_loc_conf(r, ngx_http_filter_cache_module);
+    ngx_http_filter_cache_conf_t *conf = ngx_http_get_module_loc_conf(r, ngx_http_filter_cache_module);
     if(conf->handler) {
         return NGX_DECLINED;
     } else {
